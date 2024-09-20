d4a000b5a832d74c5251f0e2da7ff3a54f33a15a
kestrelw <mail4melodie@gmail.com>
2024-09-19 20:34:40 -0400

    fixed errors...

--- src/server.js
+++ src/server.js
@@ -7,39 +7,39 @@ const imageHandler = require('./imageResponses.js');
 const port = process.env.PORT || process.env.NOD_PORT || 3000;
 
 const onRequest = (request, response) => {
-    console.log(request.url);
+  console.log(request.url);
 
-    switch (request.url) {
-        case '/':
-            htmlHandler.getIndex(request, response);
-            break;
-        case '/page2':
-            htmlHandler.getPage2(request, response);
-            break;
-        case '/rich-client.html':
-            htmlHandler.getRichClient(request, response);
-            break;
-        case '/hello':
-            textHandler.getHello(request, response);
-            break;
-        case '/time':
-            textHandler.getTime(request, response);
-            break;
-        case '/helloJSON':
-            jsonHandler.getHelloJSON(request, response);
-            break;
-        case '/timeJSON':
-            jsonHandler.getTimeJSON(request, response);
-            break;
-        case '/dankmemes':
-            imageHandler.getImage(request, response);
-            break;
-        default:
-            htmlHandler.getIndex(request, response);
-            break;
-    }
+  switch (request.url) {
+    case '/':
+      htmlHandler.getIndex(request, response);
+      break;
+    case '/page2':
+      htmlHandler.getPage2(request, response);
+      break;
+    case '/rich-client.html':
+      htmlHandler.getRichClient(request, response);
+      break;
+    case '/hello':
+      textHandler.getHello(request, response);
+      break;
+    case '/time':
+      textHandler.getTime(request, response);
+      break;
+    case '/helloJSON':
+      jsonHandler.getHelloJSON(request, response);
+      break;
+    case '/timeJSON':
+      jsonHandler.getTimeJSON(request, response);
+      break;
+    case '/dankmemes':
+      imageHandler.getImage(request, response);
+      break;
+    default:
+      htmlHandler.getIndex(request, response);
+      break;
+  }
 };
 
 http.createServer(onRequest).listen(port, () => {
-    console.log(`Listening on 127.0.0.1:${port}`);
+  console.log(`Listening on 127.0.0.1:${port}`);
 });
